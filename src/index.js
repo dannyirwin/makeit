48cff5c3572087d882af805a3a1aba7550298974
dannyirwin <dirwin123@gmail.com>
2021-07-30 10:00:40 -0600

    styling changes

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-gi;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

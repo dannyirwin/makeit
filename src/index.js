e6bec55b56f16110c9510edd20127b59963b93e9
dannyirwin <dirwin123@gmail.com>
2021-07-29 20:57:41 -0600

    deploy

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+gi;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
